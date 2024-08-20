2d14aec49f06f1f61e3f10ba0b65fa3ac607da54
Chiara Vanderlan <chvanderlan99@gmail.com>
2024-08-20 21:12:45 +0530

    Development of Web Pages

---
+++ client/src/pages/ArtiicleList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArtiicleList() {
+  return (
+    <div><h1 className='sm:text-4xl text-2xl font-bold my-6 text-grey-900'>Article
+    </h1>
+    </div>
+  )
+}
+
+export default ArtiicleList
