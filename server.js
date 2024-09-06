7961c78cc8e656f674a31e1f04d0be406676bfa5
Chiara Vanderlan <chvanderlan99@gmail.com>
2024-09-06 13:59:14 +0530

    Rewriting the comments endpoint

--- server.js
+++ server.js
@@ -21,6 +21,32 @@ const MONGO_URI = "mongodb+srv://chvanderlan99:et6IgJmxvGtT48vp@articleinfo.qhua
 // Initialize middleware
 app.use(express.json());
 
+const withDB = async(operations,res)=>{
+try{
+    const client = await MongoClient.connect(MONGO_URI);
+    const db = client.db("mernblog");
+    await operations(db)
+    client.close();
+}catch(error){
+    res.status(500).json({message:"Error connecting to database",error})
+}
+    
+}
+
+app.get('/api/articles/:name', async(req,res)=>{
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        const articlesInfo = await db
+        .collection('articles')
+        .findOne({name:articleName});
+        res.status(200).json(articlesInfo);
+
+    }, res)            
+
+  
+    
+});
+/*
 app.get('/api/articles/:name',async(req,res)=>{
     try{
         const articleName = req.params.name;
@@ -34,8 +60,7 @@ app.get('/api/articles/:name',async(req,res)=>{
     }
     
 });
-
-
+*/
 // Test routes (can be removed later)
 //app.get('/', (req, res) => res.send("Hello World!"));
 //app.post('/', (req, res) => res.send(`Hello World ${req.body.name}`));
@@ -47,8 +72,22 @@ app.post('/api/articles/:name/add-comments', (req, res) => {
     const articleName = req.params.name;
 
 
-    articlesInfo[articleName].comments.push({ username, text });
-    res.status(200).send(articlesInfo[articleName]);
+    withDB(async (db)=>{
+        const articleInfo = await db
+        .collection('articles')
+        .findOne({name:articleName })
+        await db.collection('articles').updateOne({name : articleName},{
+            $set :{
+                comments: articleInfo.comments.concat({username,text}),
+            },
+        });
+        const updateArticleInfo = await db
+        .collection('articles')
+        .findOne({name:articleName})
+        res.status(200).json(updateArticleInfo);
+    },res);
+    //articlesInfo[articleName].comments.push({ username, text });
+    //res.status(200).send(articlesInfo[articleName]);
 });
 
 // Start the server
